2908a6220379c50c6439b73b87bddc1011f6cc3c
VikramY22 <90065094+VikramY22@users.noreply.github.com>
2025-07-17 23:16:08 +0300

    Update INSERT INTO Containers.sql

---
+++ SQL запросы по заданию/Сидеры для таблиц/INSERT INTO Containers.sql
@@ -0,0 +1,20 @@
+INSERT INTO Containers (
+    Number, 
+    Type, 
+    Length, 
+    Width, 
+    Height, 
+    Weight, 
+    IsEmpty, 
+    ArrivalDate
+)
+VALUES (
+     123, 
+    'Тест', 
+    10, 
+    10.5, 
+    11, 
+    1500, 
+    1, 
+    '2025-07-14T15:30:00'
+);
